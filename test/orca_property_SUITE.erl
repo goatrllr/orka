8a47d855bfcdde3c755b6557232542d29744c527
ke <ke@kimaja.com>
2026-01-12 03:37:12 +0000

    updated tests

--- test/orca_property_SUITE.erl
+++ test/orca_property_SUITE.erl
@@ -42,17 +42,34 @@ end_per_testcase(_TestCase, _Config) ->
 
 %% @doc Property: tag index is consistent with metadata tags
 test_prop_tag_index_consistent(Config) ->
+	ok = ensure_orca_started(),
 	true = proper:quickcheck(prop_tag_index_consistent(), [{numtests, 50}]),
 	Config.
 
 %% @doc Property: property index is consistent with metadata properties
 test_prop_property_index_consistent(Config) ->
+	ok = ensure_orca_started(),
 	true = proper:quickcheck(prop_property_index_consistent(), [{numtests, 50}]),
 	Config.
+
+ensure_orca_started() ->
+	case whereis(orca) of
+		undefined ->
+			case application:ensure_all_started(orca) of
+				{ok, _} -> ok;
+				{error, {already_started, orca}} -> ok;
+				{error, Reason} -> exit({orca_start_failed, Reason})
+			end,
+			timer:sleep(50),
+			ok;
+		_ ->
+			ok
+	end.
 
 prop_tag_index_consistent() ->
 	?FORALL({Id, Tags0}, {pos_integer(), list(tag_gen())},
 		begin
+			ok = ensure_orca_started(),
 			Key = {global, service, {Id, make_ref()}},
 			Tags = lists:usort(Tags0),
 			Meta = #{tags => Tags},
@@ -68,6 +85,7 @@ prop_tag_index_consistent() ->
 prop_property_index_consistent() ->
 	?FORALL({Id, Props}, {pos_integer(), properties_gen()},
 		begin
+			ok = ensure_orca_started(),
 			Key = {global, service, {Id, make_ref()}},
 			Meta = #{properties => Props},
 			{ok, _} = orca:register(Key, self(), Meta),
